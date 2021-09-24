128f953964481df4b1b43368612f615e742c8a8b
Christopher Vouga <crvouga@gmail.com>
2021-09-24 02:37:20 -0700

    change cache strat

--- server/create-api-router.ts
+++ server/create-api-router.ts
@@ -1,5 +1,5 @@
 import apicache from "apicache";
-import { Router } from "express";
+import { Request, Response, Router } from "express";
 import {
   castTargetUrl,
   castTimeout,
@@ -10,76 +10,78 @@ import {
 } from "../screenshot";
 import { castImageType, validateImageType } from "../screenshot/imageType";
 
+const cache = apicache.middleware;
+
+const onlyStatus200 = (_req: Request, res: Response) => res.statusCode === 200;
+
+const cacheSuccesses = cache("12 hours", onlyStatus200);
+
 export const createAPIRouter = async () => {
   const browser = await createBrowser();
 
   const router = Router();
 
-  router.get(
-    "/screenshot",
-    apicache.middleware("24 hours"),
-    async (req, res) => {
-      const { url, timeout, type } = req.query;
+  router.get("/screenshot", cacheSuccesses, async (req, res) => {
+    const { url, timeout, type } = req.query;
 
-      const validationErrors = [
-        ...validateTimeout(timeout, { name: "'timeout' query param" }),
-        ...validateTargetUrl(url, { name: "'url' query param" }),
-        ...validateImageType(type, { name: "'type' query param" }),
-      ];
+    const validationErrors = [
+      ...validateTimeout(timeout, { name: "'timeout' query param" }),
+      ...validateTargetUrl(url, { name: "'url' query param" }),
+      ...validateImageType(type, { name: "'type' query param" }),
+    ];
 
-      if (validationErrors.length > 0) {
-        res.status(400).json({
-          errors: validationErrors,
-        });
-        return;
-      }
-
-      const { image, errors } = await getScreenshot({
-        imageType: castImageType(type),
-        browser,
-        timeout: castTimeout(timeout),
-        targetUrl: castTargetUrl(url),
+    if (validationErrors.length > 0) {
+      res.status(400).json({
+        errors: validationErrors,
       });
-
-      if (errors.length > 0) {
-        res
-          .status(400)
-          .json({
-            errors,
-          })
-          .end();
-
-        console.error(errors);
-
-        return;
-      }
-
-      if (!image?.data) {
-        const errors = [
-          {
-            message: "Failed to get screenshot",
-          },
-        ];
-        res
-          .status(400)
-          .json({
-            errors,
-          })
-          .end();
-
-        console.error(errors);
-
-        return;
-      }
-
-      res
-        .writeHead(200, {
-          "Content-Type": image.type,
-          "Content-Length": image.data.length,
-        })
-        .end(image.data);
+      return;
     }
-  );
+
+    const { image, errors } = await getScreenshot({
+      imageType: castImageType(type),
+      browser,
+      timeout: castTimeout(timeout),
+      targetUrl: castTargetUrl(url),
+    });
+
+    if (errors.length > 0) {
+      res
+        .status(400)
+        .json({
+          errors,
+        })
+        .end();
+
+      console.error(errors);
+
+      return;
+    }
+
+    if (!image?.data) {
+      const errors = [
+        {
+          message: "Failed to get screenshot",
+        },
+      ];
+      res
+        .status(400)
+        .json({
+          errors,
+        })
+        .end();
+
+      console.error(errors);
+
+      return;
+    }
+
+    res
+      .writeHead(200, {
+        "Content-Type": image.type,
+        "Content-Length": image.data.length,
+      })
+      .end(image.data);
+  });
 
   return router;
 };
