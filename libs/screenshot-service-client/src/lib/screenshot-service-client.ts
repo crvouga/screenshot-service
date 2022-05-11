69248f532374e1748c2b6f8b8913b332be3e71ff
Chris Vouga <49178412+crvouga@users.noreply.github.com>
2022-05-10 23:18:23 -0700

    create client lib

---
+++ libs/screenshot-service-client/src/lib/screenshot-service-client.ts
@@ -0,0 +1,54 @@
+export type IApiErrorBody = { message: string }[];
+
+export const BASE_URL = 'https://crvouga-screenshot-service.herokuapp.com';
+
+export const GET_SCREENSHOT_ENDPOINT = '/screenshot';
+
+export const API_ENDPOINT = '/api';
+
+export type IConfig = {
+  baseUrl: string;
+};
+
+export type IGetScreenshotQueryParams = {
+  timeoutMs?: string;
+  imageType?: string;
+  targetUrl?: string;
+  maxAgeMs?: string;
+};
+
+export const toGetScreenshotEndpoint = (
+  params: IGetScreenshotQueryParams,
+  config: IConfig
+): string => {
+  const queryParams = new URLSearchParams(params);
+
+  const url = `${
+    config.baseUrl
+  }${API_ENDPOINT}${GET_SCREENSHOT_ENDPOINT}?${queryParams.toString()}`;
+
+  return url;
+};
+
+export type IResult =
+  | { type: 'success'; src: string }
+  | { type: 'error'; errors: IApiErrorBody };
+
+export const fetchScreenshot = async (
+  params: IGetScreenshotQueryParams,
+  config: IConfig
+): Promise<IResult> => {
+  const response = await fetch(toGetScreenshotEndpoint(params, config));
+
+  if (response.ok) {
+    const blob = await response.blob();
+
+    const src = URL.createObjectURL(blob);
+
+    return { type: 'success', src };
+  }
+
+  const errors: IApiErrorBody = await response.json();
+
+  return { type: 'error', errors: errors };
+};
