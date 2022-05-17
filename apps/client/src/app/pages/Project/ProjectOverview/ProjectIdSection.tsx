2a03b093b2b318ea3de7176fbd9425909e250c88
Chris Vouga <49178412+crvouga@users.noreply.github.com>
2022-05-17 16:08:40 -0700

    Update ProjectIdSection.tsx

--- apps/client/src/app/pages/Project/ProjectOverview/ProjectIdSection.tsx
+++ apps/client/src/app/pages/Project/ProjectOverview/ProjectIdSection.tsx
@@ -15,9 +15,7 @@ export const ProjectIdSection = ({
         mb: 4,
       }}
     >
-      <Typography variant="h6" sx={{ mb: -1 }}>
-        project id
-      </Typography>
+      <Typography variant="h6">project id</Typography>
 
       <Typography variant="subtitle1" color="text.secondary" sx={{ mb: 2 }}>
         use this project id to get access to the backend api
