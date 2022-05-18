67a1667c1f4a2167d65dad7e0274119698105e6e
Chris Vouga <49178412+crvouga@users.noreply.github.com>
2022-05-17 20:23:21 -0700

    Update ProjectsScreenshotsTab.tsx

--- apps/client/src/app/pages/Project/ProjectScreenshots/ProjectsScreenshotsTab.tsx
+++ apps/client/src/app/pages/Project/ProjectScreenshots/ProjectsScreenshotsTab.tsx
@@ -1,8 +1,13 @@
+import { Info } from '@mui/icons-material';
 import {
   Alert,
   Box,
   CircularProgress,
   Container,
+  IconButton,
+  ImageList,
+  ImageListItem,
+  ImageListItemBar,
   Paper,
   Skeleton,
   Typography,
@@ -49,13 +54,25 @@ export const ProjectScreenshotsTab = () => {
 
         case 'success':
           return (
-            <Container maxWidth="sm">
-              {result.screenshots.map((screenshot) => (
-                <ScreenshotItem
-                  key={screenshot.projectId}
-                  screenshot={screenshot}
-                />
-              ))}
+            <Container maxWidth="md">
+              <ImageList sx={{ width: '100%', height: '100%' }} cols={3}>
+                {result.screenshots.map((screenshot) => (
+                  <ImageListItem>
+                    <ScreenshotImage
+                      screenshotId={screenshot.screenshotId}
+                      imageType={screenshot.imageType}
+                    />
+                    <ImageListItemBar
+                      title={screenshot.targetUrl}
+                      actionIcon={
+                        <IconButton sx={{ color: 'rgba(255, 255, 255, 0.54)' }}>
+                          <Info />
+                        </IconButton>
+                      }
+                    />
+                  </ImageListItem>
+                ))}
+              </ImageList>
             </Container>
           );
       }
@@ -63,71 +80,7 @@ export const ProjectScreenshotsTab = () => {
   }
 };
 
-//
-//
-//
-//
-//
-//
-//
-//
-//
-
-const ScreenshotItem = ({
-  screenshot,
-}: {
-  screenshot: Screenshots.IScreenshot;
-}) => {
-  return (
-    <Paper sx={{ mb: 2, display: 'flex' }}>
-      <Box sx={{ width: '33.33%', minWidth: '33.33%' }}>
-        <Box
-          sx={{
-            paddingTop: `${1 * 100}%`,
-            position: 'relative',
-          }}
-        >
-          <Screenshot
-            screenshotId={screenshot.screenshotId}
-            imageType={screenshot.imageType}
-          />
-        </Box>
-      </Box>
-      <Box
-        sx={{
-          p: 2,
-          overflow: 'hidden',
-        }}
-      >
-        <Field label="target url" value={screenshot.targetUrl} />
-        <Field label="timeout ms" value={String(screenshot.timeoutMs)} />
-        <Field label="image type" value={screenshot.imageType} />
-      </Box>
-    </Paper>
-  );
-};
-
-const Field = ({ label, value }: { label: string; value: string }) => {
-  return (
-    <Box sx={{ mb: 1 }}>
-      <Typography variant="subtitle1" color="text.secondary">
-        {label}
-      </Typography>
-      <Typography
-        component="div"
-        sx={{
-          overflow: 'hidden',
-          textOverflow: 'ellipsis',
-          whiteSpace: 'nowrap',
-        }}
-      >
-        {value}
-      </Typography>
-    </Box>
-  );
-};
-
-const Screenshot = ({
+const ScreenshotImage = ({
   screenshotId,
   imageType,
 }: {
@@ -140,64 +93,66 @@ const Screenshot = ({
   );
 
   return (
-    <Box
-      sx={{
-        position: 'absolute',
-        left: 0,
-        top: 0,
-        width: '100%',
-        height: '100%',
-      }}
-    >
-      {query.status === 'idle' && (
-        <Skeleton variant="rectangular" width="100%" height="100%" />
-      )}
+    <Box sx={{ position: 'relative', paddingTop: '100%' }}>
+      <Box
+        sx={{
+          position: 'absolute',
+          left: 0,
+          top: 0,
+          width: '100%',
+          height: '100%',
+        }}
+      >
+        {query.status === 'idle' && (
+          <Skeleton variant="rectangular" width="100%" height="100%" />
+        )}
 
-      {query.status === 'loading' && (
-        <Skeleton variant="rectangular" width="100%" height="100%" />
-      )}
+        {query.status === 'loading' && (
+          <Skeleton variant="rectangular" width="100%" height="100%" />
+        )}
 
-      {query.status === 'error' && (
-        <Alert
-          sx={{
-            justifyContent: 'center',
-            alignItems: 'center',
-            width: '100%',
-            height: '100%',
-          }}
-          severity="error"
-        >
-          Failed to load screenshot
-        </Alert>
-      )}
+        {query.status === 'error' && (
+          <Alert
+            sx={{
+              justifyContent: 'center',
+              alignItems: 'center',
+              width: '100%',
+              height: '100%',
+            }}
+            severity="error"
+          >
+            Failed to load screenshot
+          </Alert>
+        )}
 
-      {query.status === 'success' && (
-        <>
-          {query.data.type === 'error' && (
-            <Alert
-              sx={{
-                justifyContent: 'center',
-                alignItems: 'center',
-                width: '100%',
-                height: '100%',
-              }}
-              severity="error"
-            >
-              Failed to load screenshot
-            </Alert>
-          )}
+        {query.status === 'success' && (
+          <>
+            {query.data.type === 'error' && (
+              <Alert
+                sx={{
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  width: '100%',
+                  height: '100%',
+                }}
+                severity="error"
+              >
+                Failed to load screenshot
+              </Alert>
+            )}
 
-          {query.data.type === 'success' && (
-            <img
-              alt="..."
-              width="100%"
-              height="100%"
-              src={query.data.src}
-              style={{ objectFit: 'cover' }}
-            />
-          )}
-        </>
-      )}
+            {query.data.type === 'success' && (
+              <img
+                alt="..."
+                width="100%"
+                height="100%"
+                src={query.data.src}
+                style={{ objectFit: 'cover' }}
+              />
+            )}
+          </>
+        )}
+      </Box>
     </Box>
   );
 };
