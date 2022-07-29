ce6557e704288fc80ede3aecfc0dbf122e5df079
Chris Vouga <49178412+crvouga@users.noreply.github.com>
2022-07-28 18:33:46 -0700

    Update ProjectRequestTable.tsx

--- apps/client/src/app/pages/Project/ProjectUsage/ProjectRequestTable.tsx
+++ apps/client/src/app/pages/Project/ProjectUsage/ProjectRequestTable.tsx
@@ -1,238 +1,99 @@
-import { ArrowLeft, ArrowRight } from '@mui/icons-material';
-import {
-  Box,
-  Button,
-  CircularProgress,
-  MenuItem,
-  Paper,
-  Select,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow, Typography
-} from '@mui/material';
-import { Data } from '@screenshot-service/screenshot-service';
+import { Paper, Box, MenuItem, Select, Toolbar, Typography } from '@mui/material';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import { Data } from "@screenshot-service/screenshot-service";
 import { CaptureScreenshotRequest } from '@screenshot-service/shared';
 import { useEffect, useState } from 'react';
 import { dataAccess } from '../../../data-access';
 
-const PAGE_SIZE = 5;
+const columns: GridColDef[] = [
+  { field: "requestId", headerName: "ID" },
+  { field: "targetUrl", headerName: "Target Url", resizable: true },
+  { field: "createdAt", headerName: "Created At", resizable: true },
+  { field: "status", headerName: "Status", },
+  { field: "strategy", headerName: "Strategy" },
+  { field: "delaySec", headerName: "Delay (sec)" },
+  { field: "imageType", headerName: "Image Type" },
+  { field: "originUrl", headerName: "Origin Url", resizable: true },
+];
 
-type Order = 'OldestFirst' | 'NewestFirst';
+const PAGE_SIZE = 5
 
-const fetchPage = async ({
-  projectId,
-  pageIndex,
-  order,
-}: {
-  projectId: Data.ProjectId.ProjectId;
-  pageIndex: number;
-  order: Order;
-}): Promise<CaptureScreenshotRequest[]> => {
+type Order = 'OldestFirst' | 'NewestFirst'
+
+const fetchPage = async ({ projectId, pageIndex, order }: { projectId: Data.ProjectId.ProjectId, pageIndex: number, order: Order }): Promise<CaptureScreenshotRequest[]> => {
   const result = await dataAccess.captureScreenshotRequest.findMany({
     pageSize: PAGE_SIZE,
     page: pageIndex,
     projectId,
-    order,
-  });
+    order
+  })
 
   if (result.type === 'Err') {
-    return [];
+    return []
   }
 
-  return result.value;
-};
-
-type Page =
-  | { type: 'Loading' }
-  | { type: 'Succeeded'; data: CaptureScreenshotRequest[] };
+  return result.value
+}
 
 export const ProjectRequestTable = ({ projectId }: { projectId: Data.ProjectId.ProjectId }) => {
-  const [order, setOrder] = useState<Order>('NewestFirst');
-  const [pageIndex, setPageIndex] = useState(0);
-  const [pages, setPages] = useState<{ [pageIndex: number]: Page }>({});
-
-  const onPrev = () => {
-    setPageIndex((pageIndex) => Math.max(0, pageIndex - 1));
-  };
-
-  const onNext = () => {
-    setPageIndex((pageIndex) => Math.max(0, pageIndex + 1));
-  };
+  const [order, setOrder] = useState<Order>("NewestFirst")
+  const [pageIndex, setPageIndex] = useState(0)
+  const [pages, setPages] = useState<CaptureScreenshotRequest[][]>([])
+  const [status, setStatus] = useState<"idle" | "loading">("idle")
 
   useEffect(() => {
-    const currentPageIndex = pageIndex;
-    if (pages[currentPageIndex] === undefined) {
-      setPages((pages) => ({
-        ...pages,
-        [currentPageIndex]: { type: 'Loading' },
-      }));
-
-      fetchPage({
-        pageIndex: currentPageIndex,
-        order,
-        projectId,
-      }).then((page) => {
-        setPages((pages) => ({
-          ...pages,
-          [currentPageIndex]: { type: 'Succeeded', data: page },
-        }));
-      });
+    if (pageIndex >= pages.length - 1) {
+      setStatus("loading")
+      fetchPage({ pageIndex, order, projectId }).then(page => {
+        setStatus("idle")
+        setPages(pages => ([...pages, page]))
+      })
     }
-  }, [pageIndex, order, pages, projectId]);
+  }, [pageIndex, order, pages.length, projectId])
 
   useEffect(() => {
-    setPageIndex(0);
-    setPages([]);
-  }, [order]);
+    setPageIndex(0)
+    setPages([])
+  }, [order])
 
-  const page = pages[pageIndex] ?? { type: 'Loading' };
+  const rows = pages.flatMap(page => page)
 
-  const canFetchNextPage =
-    page.type === 'Succeeded' && page.data.length >= PAGE_SIZE;
-
-  return (
-
-    <Paper sx={{ p: 2, marginBottom: 4 }}>
-      <Box sx={{ display: "flex", alignItems: "center", marginBottom: 2 }}>
-        <Typography variant="h6" sx={{ flex: 1 }}>
-          Requests
-        </Typography>
-        <Select
-          id="order-select"
-          labelId="order-select-label"
-          value={order}
-          size="small"
-          onChange={(event) => {
-            const orderNew = event.target.value;
-            if (orderNew === 'OldestFirst' || orderNew === 'NewestFirst') {
-              setOrder(orderNew);
-              return;
-            }
-          }}
-        >
-          <MenuItem value={'OldestFirst'}>{'Oldest First'}</MenuItem>
-          <MenuItem value={'NewestFirst'}>{'Newest First'}</MenuItem>
-        </Select>
-      </Box>
-
-      <TableContainer
-        sx={{ position: 'relative', height: 345, maxHeight: 345 }}
-        component={Paper}
-      >
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>ID</TableCell>
-              <TableCell>Target Url</TableCell>
-              <TableCell>Created At</TableCell>
-              <TableCell>Status</TableCell>
-              <TableCell>Strategy</TableCell>
-              <TableCell>Delay (sec)</TableCell>
-              <TableCell>Image Type</TableCell>
-              <TableCell>Origin Url</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {page.type === 'Loading' && (
-              <Box
-                component="tr"
-                sx={{
-                  position: 'absolute',
-                  top: 0,
-                  left: 0,
-                  bottom: 0,
-                  right: 0,
-                  width: '100%',
-                  height: '100%',
-                  display: 'flex',
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                }}
-              >
-                <CircularProgress disableShrink />
-              </Box>
-            )}
-
-            {page.type === 'Succeeded' && (
-              <>
-                {page.data.length === 0 && (
-                  <Box
-                    component="tr"
-                    sx={{
-                      position: 'absolute',
-                      top: 0,
-                      left: 0,
-                      bottom: 0,
-                      right: 0,
-                      width: '100%',
-                      height: '100%',
-                      display: 'flex',
-                      alignItems: 'center',
-                      justifyContent: 'center',
-                    }}
-                  >
-                    <Typography>No rows</Typography>
-                  </Box>
-                )}
-
-                {page.data.map((row) => (
-                  <TableRow key={row.requestId}>
-                    <Cell value={row.requestId} />
-                    <Cell value={row.targetUrl} />
-                    <Cell value={row.createdAt} />
-                    <Cell value={row.status} />
-                    <Cell value={row.strategy} />
-                    <Cell value={String(row.delaySec)} />
-                    <Cell value={row.imageType} />
-                    <Cell value={row.originUrl} />
-                  </TableRow>
-                ))}
-              </>
-            )}
-          </TableBody>
-        </Table>
-      </TableContainer>
-
-      <Box
-        sx={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          width: '100%',
-          marginTop: 1,
+  return <Paper sx={{ p: 2, marginBottom: 4 }}>
+    <Toolbar disableGutters>
+      <Typography variant="h6" sx={{ flex: 1 }}>
+        Requests
+      </Typography>
+      <Select
+        id="order-select"
+        labelId="order-select-label"
+        value={order}
+        size="small"
+        onChange={event => {
+          const orderNew = event.target.value
+          if (orderNew === 'OldestFirst' || orderNew === 'NewestFirst') {
+            setOrder(orderNew)
+            return
+          }
         }}
       >
-        <Button
-          disabled={pageIndex === 0}
-          size="large"
-          startIcon={<ArrowLeft />}
-          onClick={onPrev}
-        >
-          Prev
-        </Button>
-        <Typography sx={{ marginX: 2 }} variant="h6">
-          {pageIndex + 1}
-        </Typography>
-        <Button
-          size="large"
-          endIcon={<ArrowRight />}
-          onClick={onNext}
-          disabled={!canFetchNextPage}
-        >
-          Next
-        </Button>
-      </Box>
-    </Paper>
-  );
-};
+        <MenuItem value={"OldestFirst"}>{"Oldest First"}</MenuItem>
+        <MenuItem value={"NewestFirst"}>{"Newest First"}</MenuItem>
+      </Select>
+    </Toolbar>
 
-const Cell = ({ value }: { value: string }) => {
-  return (
-    <TableCell sx={{ maxWidth: 100, width: 100 }}>
-      <Typography noWrap>{value}</Typography>
-    </TableCell>
-  );
+    <Box sx={{ height: 400, width: '100%' }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        page={pageIndex}
+        pageSize={PAGE_SIZE}
+        rowsPerPageOptions={[PAGE_SIZE]}
+        pagination
+        onPageChange={setPageIndex}
+        getRowId={row => row.requestId}
+        disableSelectionOnClick
+        loading={status === 'loading'}
+      />
+    </Box>
+  </Paper >
 };
