1040e9678908671c1474ace6a51d1c50549f8ddc
Chris Vouga <49178412+crvouga@users.noreply.github.com>
2022-07-29 05:03:07 -0700

    Update configuration.ts

--- libs/shared/src/lib/configuration.ts
+++ libs/shared/src/lib/configuration.ts
@@ -1,6 +1,6 @@
 export const configuration = {
-  MAX_DAILY_CAPTURE_SCREENSHOT_REQUESTS: 100,
+  MAX_DAILY_CAPTURE_SCREENSHOT_REQUESTS: 1000,
   MAX_PROJECT_COUNT: 3,
 };
 
-export const CAPTURE_SCREENSHOT_RATE_LIMIT_ERROR_MESSAGE = `Daily request limit has been reached. A max of ${configuration.MAX_DAILY_CAPTURE_SCREENSHOT_REQUESTS} capture screenshot requests are allowed daily.`;
+export const CAPTURE_SCREENSHOT_RATE_LIMIT_ERROR_MESSAGE = `Daily request limit has been reached. A max of ${configuration.MAX_DAILY_CAPTURE_SCREENSHOT_REQUESTS} capture screenshot requests are allowed.`;
