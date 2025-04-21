9a58f2ec2e327897fb605706638c5b9f70e7ad12
Chris Vouga <crvouga@gmail.com>
2025-04-20 20:38:34 -0700

    Refactor TRPC client initialization by replacing createTRPCClient with createTRPCProxyClient for improved type safety and functionality.

--- libs/shared/src/lib/trpc-client.ts
+++ libs/shared/src/lib/trpc-client.ts
@@ -1,7 +1,7 @@
 import type { AppRouter } from '@screenshot-service/server-trpc';
-import { createTRPCClient, httpBatchLink } from '@trpc/client';
+import { createTRPCProxyClient, httpBatchLink } from '@trpc/client';
 
-export const trpcClient = createTRPCClient<AppRouter>({
+export const trpcClient = createTRPCProxyClient<AppRouter>({
   links: [
     httpBatchLink({
       url: 'http://localhost:8000/trpc',
